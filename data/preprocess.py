0bce0cde47ac38b306ff6afb62da37eeb85ed489
Raymond Li <raymond.b.li8@gmail.com>
2021-11-17 23:40:54 -0500

    webscraper

---
+++ data/preprocess.py
@@ -0,0 +1,8 @@
+import sys, os
+import csv
+from pathlib import Path
+
+for root, subdirectories, files in os.walk("./"):
+    for subdirectory in subdirectories:
+        for root, subdirectories, files in os.walk()
+
